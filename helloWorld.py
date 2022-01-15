621f4103d13e5fe29b6ffa1db0316328951ea547
nolanjannotta <anjannotta@verizon.net>
2022-01-15 11:52:48 -0800

    first commit

---
+++ helloWorld.py
@@ -0,0 +1,39 @@
+from PIL import Image, ImageDraw, ImageFilter
+import time
+import epd7in5_V2
+import logging
+import os
+picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__>
+
+epd = epd7in5_V2.EPD()
+epd.init()
+
+# clear the screen
+epd.Clear()
+
+
+# Horizontal image
+Himage = Image.new('1', (epd7in5_V2.EPD_WIDTH, epd7in5_V2.EPD_HEIGHT), 255)
+draw = ImageDraw.Draw(Himage)
+draw.text((0, 0), "hello world", fill=0)
+draw.text((0, 10), "whats poppin?", fill=0)
+draw.text((0, 20), "brand new whip just hopped in", fill=0)
+
+Hbuf = epd.getbuffer(Himage)
+
+
+# Display buffer
+epd.display(Hbuf)
+
+# Set display in deep sleep
+time.sleep(10)
+#Vertical image
+
+Limage = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
+draw = ImageDraw.Draw(Limage)
+draw.text((0, 0), "oi cunt", fill=0)
+draw.text((0, 10), "whats poppin?", fill=0)
+draw.text((0, 20), "brand new whip just hopped in", fill=0)
+Lbuf = epd.getbuffer(Limage)
+epd.display(Lbuf)
+epd.sleep()
