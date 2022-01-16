2ab679b1ce281f630bdd88907c0e5684e9aa0885
nolanjannotta <anjannotta@verizon.net>
2022-01-15 22:55:37 -0800

    weather

--- helloWorld.py
+++ helloWorld.py
@@ -1,6 +1,5 @@
 from PIL import Image,ImageDraw,ImageFont
 from datetime import date
-import time
 import epd7in5_V2
 import logging
 import os
@@ -26,20 +25,20 @@ def main():
 
 
     # Horizontal image
-    Himage = Image.new('1', (epd7in5_V2.EPD_WIDTH, epd7in5_V2.EPD_HEIGHT), 255)
-    draw = ImageDraw.Draw(Himage)
-    draw.text((0, 0), "hello world", fill=0)
-    draw.text((0, 10), "whats poppin?", fill=0)
-    draw.text((0, 20), "brand new whip just hopped in", fill=0)
+    # Himage = Image.new('1', (epd7in5_V2.EPD_WIDTH, epd7in5_V2.EPD_HEIGHT), 255)
+    # draw = ImageDraw.Draw(Himage)
+    # draw.text((0, 0), "hello world", fill=0)
+    # draw.text((0, 10), "whats poppin?", fill=0)
+    # draw.text((0, 20), "brand new whip just hopped in", fill=0)
 
-    Hbuf = epd.getbuffer(Himage)
+    # Hbuf = epd.getbuffer(Himage)
 
 
-    # Display buffer
-    epd.display(Hbuf)
+    # # Display buffer
+    # epd.display(Hbuf)
 
     # Set display in deep sleep
-    time.sleep(3)
+    # time.sleep(3)
     #Vertical image
 
     currentWeather = weather.Weather()
